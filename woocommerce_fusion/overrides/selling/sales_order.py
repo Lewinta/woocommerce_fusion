340c760920d338c96f66eb35fa6e8f76404971d6
Lewin Villar <lewin.villar@gmail.com>
2025-06-14 13:04:22 -0400

    Implement auto-selection of warehouses based on item availability in sales orders

--- woocommerce_fusion/overrides/selling/sales_order.py
+++ woocommerce_fusion/overrides/selling/sales_order.py
@@ -41,7 +41,120 @@ class CustomSalesOrder(SalesOrder):
 		super(CustomSalesOrder, self).validate()
 		self.copy_address()
 		self.set_default_mode_of_delivery()
-	
+
+	# override
+	def before_insert(self):
+		self.auto_select_warehouse(replace=True)
+
+	def auto_select_warehouse(self, replace=False):
+		# if order is from an online store auto-select warehouse for each item
+		# based on their availability (if there is stock)
+		if self.fulfillment_method not in {"MFN", "eBay", "WooCommerce"}:
+			return # only for online orders (add more if needed)
+
+		for item in self.items.copy(): # use copy to avoid modifying the original list while iterating
+			if item.warehouse and not replace:
+				continue
+
+			warehouse = self._get_warehouse_based_on_availability(item)
+			if warehouse:
+				item.warehouse = warehouse
+			else:
+				self._distribute_based_on_availability(item)
+			
+	def _distribute_based_on_availability(self, item):
+		# check to see if the item is availabe at different locations (warehouses)
+		# if so, we need to select the first and assign the available qty at that warehouse
+		# and then duplicate the item line for each warehouse until the qty is met
+		warehouses = frappe.get_all(
+			"Bin",
+			fields=["warehouse", "actual_qty"],
+			filters={
+				"item_code": item.item_code,
+				"actual_qty": [">", 0]
+			},
+			order_by="actual_qty DESC",
+		)
+
+		# we are working on the assumption that the warehouse available qty is lesser than the item qty
+		if warehouses:
+			original_qty = item.qty
+
+			for idx, warehouse in enumerate(warehouses):
+				# first warehouse will be mapped to the current item
+				if idx == 0:
+					item.warehouse = warehouse.warehouse
+					item.qty = min(original_qty, warehouse.actual_qty)
+					original_qty -= item.qty
+					if original_qty <= 0: # unlikely to happen but just in case
+						break
+
+					continue
+				# for subsequent warehouses, we need to create a new item row
+				# with the warehouse and the qty available at that warehouse
+				# if the original qty is already met, we can break
+
+				if original_qty <= 0:
+					break
+
+				# create a new item row for each warehouse
+				new_item = frappe.copy_doc(item)
+				new_item.docstatus = 0  # reset docstatus to draft
+				new_item.warehouse = warehouse.warehouse
+				new_item.qty = min(original_qty, warehouse.actual_qty)
+				new_item.stock_qty = new_item.qty * new_item.conversion_factor
+				new_item.amount = new_item.qty * new_item.rate
+				new_item.base_amount = new_item.qty * new_item.base_rate
+				self.append("items", new_item).db_insert()
+
+				# reduce the original qty by the qty assigned to the new item
+				original_qty -= new_item.qty
+
+			if original_qty > 0:
+				# if there is still some qty left, we can log an error
+				frappe.log_error(
+					_("Not enough stock available for item {0}").format(item.item_code),
+					_("Sales Order {0}").format(self.name)
+				)
+			else:
+				# if we have successfully assigned all the qty, we can remove the original item
+				...
+
+
+			# refresh idxs in because we have added new items
+			for idx, item in enumerate(self.items):
+				item.idx = idx + 1
+
+
+			self.calculate_taxes_and_totals()
+		else:
+			frappe.log_error(
+				_("No stock available for item {0}").format(item.item_code),
+				_("Sales Order {0}").format(self.name)
+			)
+ 		
+
+	def _get_warehouse_based_on_availability(self, item):
+		"""
+		Returns the warehouse with the highest stock for the item.
+		If no stock is available, returns None.
+		"""
+		if not item.item_code:
+			return None
+
+		warehouses = frappe.get_all(
+			"Bin",
+			fields=["warehouse"],
+			filters={
+				"item_code": item.item_code,
+				"actual_qty": [">", item.qty or 0],
+			},
+			order_by="actual_qty DESC", # Ideally this should be FIFO
+			limit=1
+		)
+
+		return warehouses[0].warehouse if warehouses else None
+
 	def on_update_after_submit(self):
 		self.set_default_mode_of_delivery()
 	
@@ -160,4 +273,3 @@ def get_woocommerce_order(woocommerce_server, woocommerce_id):
 	wc_order = frappe.get_doc({"doctype": "WooCommerce Order", "name": wc_order_name})
 	wc_order.load_from_db()
 	return wc_order
-
